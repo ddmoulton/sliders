0547e2e9c55ba2ce6ba850afd8dce6bab0b539ac
ddmoulton <ddmoulton@vwc.edu>
2014-04-07 10:32:26 -0400

    Setting the grid up

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
-function splashResize() {  
-    var windowSize = $(window).height();
-    console.log(windowSize);
-    $('.splash').css("height", windowSize); 
-}
-function sectionResize() {
-    var windowSize = $(window).height();
-    console.log(windowSize);
-    $('section').css("height", windowSize); 
-}
-$(document).ready(function() {
-    splashResize();
-    sectionResize();
-});
-
-$( window ).resize(function() {
-    splashResize();
-    sectionResize();
-});
+//function splashResize() {  
+//    var windowSize = $(window).height();
+//    console.log(windowSize);
+//    $('.splash').css("height", windowSize); 
+//}
+//function sectionResize() {
+//    var windowSize = $(window).height();
+//    console.log(windowSize);
+//    $('section').css("height", windowSize); 
+//}
+//$(document).ready(function() {
+//    splashResize();
+//    sectionResize();
+//});
+//
+//$( window ).resize(function() {
+//    splashResize();
+//    sectionResize();
+//});
