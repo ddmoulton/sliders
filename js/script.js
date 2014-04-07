d793f267897bb417d4d0c571403dbe0fb31c7dc8
ddmoulton <ddmoulton@vwc.edu>
2014-04-07 10:26:12 -0400

    Actual Initial Commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function splashResize() {  
+    var windowSize = $(window).height();
+    console.log(windowSize);
+    $('.splash').css("height", windowSize); 
+}
+function sectionResize() {
+    var windowSize = $(window).height();
+    console.log(windowSize);
+    $('section').css("height", windowSize); 
+}
+$(document).ready(function() {
+    splashResize();
+    sectionResize();
+});
+
+$( window ).resize(function() {
+    splashResize();
+    sectionResize();
+});
