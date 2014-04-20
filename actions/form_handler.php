ee49e6c394ec8259c6a839cf1a0c268c20950939
ddmoulton <ddmoulton@vwc.edu>
2014-04-20 17:37:47 -0400

    Form handler php script working
    grabs user data, converts it into lat/lon location and saves it to the
    database

--- actions/form_handler.php
+++ actions/form_handler.php
@@ -1,27 +1,36 @@
 
 
 <?php
-	$truckLocation = "5133 Brockie Street, Virginia Beach, VA, United States";
+
+
+
+
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$address = $_POST['address'];
+	$within = $_POST['within'];
+
+	// $truckLocation = "5133 Brockie Street, Virginia Beach, VA, United States";
  
-  	$address = array( 1 => "513 Bunker Drive, Virginia Beach, VA, United States",
-  						2 => "230 Mannings Lane, Virginia Beach, VA, United States");
+  	// $address = "5133 Brockie Street, Virginia Beach, VA, United States";
 
 
-  $request_url = "http://maps.googleapis.com/maps/api/geocode/xml?address=".$truckLocation."&sensor=true";
-  $xml = simplexml_load_file($request_url) or die("url not loading");
-  $status = $xml->status;
-  if ($status=="OK") {
-      $truckLat = $xml->result->geometry->location->lat;
-      $truckLon = $xml->result->geometry->location->lng;
-      $truckLatLng = "$truckLat,$truckLon";
-  }
+
+  // $request_url = "http://maps.googleapis.com/maps/api/geocode/xml?address=".$truckLocation."&sensor=true";
+  // $xml = simplexml_load_file($request_url) or die("url not loading");
+  // $status = $xml->status;
+  // if ($status=="OK") {
+  //     $truckLat = $xml->result->geometry->location->lat;
+  //     $truckLon = $xml->result->geometry->location->lng;
+  //     $truckLatLng = "$truckLat,$truckLon";
+  // }
 
 
-foreach ($address as $val) {
+// foreach ($address as $val) {
     
 	
-	
-	  $request_url = "http://maps.googleapis.com/maps/api/geocode/xml?address=".$val."&sensor=true";
+
+	  $request_url = "http://maps.googleapis.com/maps/api/geocode/xml?address=".$address."&sensor=true";
 	  $xml = simplexml_load_file($request_url) or die("url not loading");
 	  $status = $xml->status;
 	  if ($status=="OK") {
@@ -29,42 +38,51 @@ foreach ($address as $val) {
 	      $userLon = $xml->result->geometry->location->lng;
 	      $userLatLng = "$userLat,$userLon";
 	  }
-	  else {
-
-	  }
-	  
-	  echo "Truck Address: ".$truckLocation;
-
-	   echo "<br />";
-	  echo "Other Location: ".$val;
-
-	  echo "<br />";
 
 
-	  $distance = distance($truckLat, $truckLon, $userLat, $userLon);
-	  echo "Distance: ";
-	  echo $distance;
-	  echo "<br />";
-		echo "<br />";
-	  echo "<br />";
-}
+
+// function distance($lat1, $lon1, $lat2, $lon2) {
 
 
-function distance($lat1, $lon1, $lat2, $lon2) {
+// 	$lat1 = floatval($lat1);
+// 	$lat2 = floatval($lat2);
+// 	$lon1 = floatval($lon1);
+// 	$lon2 = floatval($lon2);
 
-
-	$lat1 = floatval($lat1);
-	$lat2 = floatval($lat2);
-	$lon1 = floatval($lon1);
-	$lon2 = floatval($lon2);
-
-  $theta = $lon1 - $lon2;
-  $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
-  $dist = acos($dist);
-  $dist = rad2deg($dist);
-  $miles = $dist * 60 * 1.1515;
+//   $theta = $lon1 - $lon2;
+//   $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
+//   $dist = acos($dist);
+//   $dist = rad2deg($dist);
+//   $miles = $dist * 60 * 1.1515;
   
-    return ($miles * 1.609344);
-}
+//     return ($miles * 1.609344);
+// }
+
+?>
+
+
+
+
+<?php
+
+$addr= $address;
+$lat = $userLat;
+$lon = $userLon;
+
+
+// setup the database connect
+$cxn = mysql_connect('localhost', 'root');
+if (!$cxn)
+    exit;
+mysql_select_db('test', $cxn);
+
+
+// lets setup our insert query
+$sql = "INSERT INTO test (name, email, address, lat, lon, within) VALUES ('".$name."', '".$email."', '".$addr."', '".$lat."', '".$lon."', '".$within."' )";
+
+// lets run our query
+echo $sql;
+$result = mysql_query($sql, $cxn);
+
 
 ?>
